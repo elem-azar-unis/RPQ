2cccabddee6c986276422a44b621952bd11039d0
elem-azar-unis <1056835162@qq.com>
2016-01-27 14:29:50 +0800

    kernel priority queue for client
    Insert, Alter, Delete max, Get max

--- RPQ/src/kernel/ClientPriorityQueue.java
+++ RPQ/src/kernel/ClientPriorityQueue.java
@@ -3,29 +3,17 @@ package kernel;
 import java.util.Arrays;
 
 /**
- * The kernel priority queue for server. It uses ElementTable to find an element in constant time.
+ * The kernel priority queue for client. It uses ElementTable to find an element in constant time.
  * <p>This is an unbounded priority queue based on heap. This is a maximam priority queue.
  * The priority is provided by user difined Comparable.
- * <p>The basic operations: Insert, Alter, Delete max, Get max, Append
- * <p>Insert and alter don't take immediate effect. They have two steps. 
- * First is to update the desierd value. 
- * Then, append this deiserd value and fix the heap.
+ * <p>The basic operations: Insert, Alter, Delete max, Get max
  * <p>Also can get or remove one element given the identifier.
  * @param <K> The type of identifier.
  * @param <V> The type of value of priority.
  * @param <T> The type of element in the priority queue.
  */
-public class ServerPriorityQueue<K,V extends Comparable<V>,T extends Element<K,V>>
+public class ClientPriorityQueue<K,V extends Comparable<V>,T extends Element<K,V>>
 {
-	class Node
-	{
-		T element;
-		/**
-		 * Used only by the server RPQ. The change of the priority that is to be applied in the future.
-		 */
-		V desired=null;
-		public Node(T element){this.element=element;}
-	}
 	private static final int INITIAL_CAPACITY=8;
 	/**
      * Priority queue represented as a balanced binary heap: the two
@@ -35,7 +23,7 @@ public class ServerPriorityQueue<K,V extends Comparable<V>,T extends Element<K,V
      * The element with the highest value is in queue[0], assuming the queue is nonempty.
      */	
 	@SuppressWarnings("unchecked")
-	private Node[] elements=(ServerPriorityQueue<K, V, T>.Node[]) new Object[INITIAL_CAPACITY];
+	private T[] elements=(T[]) new Object[INITIAL_CAPACITY];
 	/**
      * The size of the Priority Queue (the number of elements it contains).
      *
@@ -60,18 +48,18 @@ public class ServerPriorityQueue<K,V extends Comparable<V>,T extends Element<K,V
      */
     private void shiftUP(int k)
     {
-    	Node e=elements[k];
+    	T e=elements[k];
     	while(k>0)
     	{
     		int parent=(k-1)>>>1;
-    		if(e.element.compareTo(elements[parent].element)<=0)
+    		if(e.compareTo(elements[parent])<=0)
     			break;
     		elements[k]=elements[parent];
-    		elements[k].element.index=k;
+    		elements[k].index=k;
     		k=parent;
     	}
     	elements[k]=e;
-    	e.element.index=k;
+    	e.index=k;
     }
     /**
      * Fix the heap from position k (let's call it x). Maintaining heap invariant by
@@ -80,26 +68,25 @@ public class ServerPriorityQueue<K,V extends Comparable<V>,T extends Element<K,V
      */
     private void shiftDown(int k)
     {
-    	Node e=elements[k];
+    	T e=elements[k];
     	int half=size>>>1;            // loop while a non-leaf
     	while(k<half)
     	{
     		int child=(k<<1)+1;       // assume left child is largest
     		int right=child+1;
-    		if(right<size && elements[child].element.compareTo(elements[right].element)<0)
+    		if(right<size && elements[child].compareTo(elements[right])<0)
     			child=right;
-    		if(e.element.compareTo(elements[child].element)>=0)
+    		if(e.compareTo(elements[child])>=0)
     			break;
     		elements[k]=elements[child];
-    		elements[k].element.index=k;
+    		elements[k].index=k;
     		k=child;
     	}
     	elements[k]=e;
-    	e.element.index=k;
+    	e.index=k;
     }
     /**
      * Insert a new element in the priority queue.
-     * You need to append it for the insertion to take place.
      * @param e The element to be inserted.
      */
     public void insert(T e)
@@ -107,22 +94,29 @@ public class ServerPriorityQueue<K,V extends Comparable<V>,T extends Element<K,V
     	table.add(e);
     	if(size==elements.length)
     		grow();
-    	elements[size]=new Node(e);
-    	elements[size].desired=e.priority;
-    	e.priority=null;
+    	elements[size]=e;
     	e.index=size;
     	size++;
+    	shiftUP(e.index);
     }
     /**
      * Change the proiroty of the element having the identifier "key".
-     * You need to  append it for the alter to take place.
      * @param key the identifier.
      * @param value the disierd value.
      */
     public void alter(K key,V value)
     {
     	int index=table.get(key).index;
-    	elements[index].desired=value;
+    	if(elements[index].priority.compareTo(value)>0)
+    	{
+    		elements[index].priority=value;
+    		shiftDown(index);
+    	}
+    	else
+    	{
+    		elements[index].priority=value;
+    		shiftUP(index);
+    	}
     }
     /**
      * Get the element having the max priority.
@@ -130,7 +124,7 @@ public class ServerPriorityQueue<K,V extends Comparable<V>,T extends Element<K,V
      */
     public T getMax()
     {
-    	return size==0? null : elements[0].element;
+    	return size==0? null : elements[0];
     }
     /**
      * Delete the element having the max priority.
@@ -139,27 +133,12 @@ public class ServerPriorityQueue<K,V extends Comparable<V>,T extends Element<K,V
     public T deleteMax()
     {
     	if(size==0)return null;
-    	T rtn=elements[0].element;
+    	T rtn=elements[0];
     	size--;
     	elements[0]=elements[size];
     	shiftDown(0);
     	table.remove(rtn);
     	return rtn;
-    }
-    /**
-     * Write the desierd priority.
-     * @param tar the element to be changed.
-     */
-    public void append(T tar)
-    {
-    	int index=tar.index;
-    	V pre=tar.priority;
-    	tar.priority=elements[index].desired;
-    	elements[index].desired=null;
-    	if(pre==null || pre.compareTo(tar.priority)<0)
-    		shiftUP(index);
-    	else 
-    		shiftDown(index);
     }
     /**
      * Get the element having the identifier "key"
@@ -182,7 +161,7 @@ public class ServerPriorityQueue<K,V extends Comparable<V>,T extends Element<K,V
     	table.remove(rtn);
     	size--;
     	elements[index]=elements[size];
-    	if(rtn.compareTo(elements[index].element)>0)
+    	if(rtn.compareTo(elements[index])>0)
     		shiftDown(index);
     	else
 			shiftUP(index);
